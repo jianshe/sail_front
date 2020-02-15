0b94dff9cd4a76d3987bcc546d624397f3061fe7
jianshe <zhangshi789wei@163.com>
2020-02-16 00:44:25 +0800

    add pagination.js and new page

---
+++ src/pages/about-us/index.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/js/bootstrap.min.js";
+import "popper.js";
+import "../../util/bootnavbar.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../styles/index.scss";
+import "./index.scss";
+$(function() {
+  $("#layout-header").bootnavbar();
+});
---
+++ src/pages/library-detail/index.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/js/bootstrap.min.js";
+import "popper.js";
+import "../../util/bootnavbar.js";
+import "../../util/jquery-paginator.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../styles/index.scss";
+import "./index.scss";
+$(function() {
+  $("#layout-header").bootnavbar();
+  $("#myTab a").on("click", function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+  $('#pagination').Paging({pagesize:10,count:85,toolbar:true,callback: function(current, pagesize, pagecount) {
+    console.log(current, pagesize, pagecount)
+  }});
+});
